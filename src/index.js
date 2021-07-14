70e78db1878818bd530b4e168e8b061dfeafce66
matheus souza <ruivorobado@gmail.com>
2021-07-14 12:50:13 -0300

    projeto finalizado

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors")
+const { uuid } = require("uuidv4");
+const port = process.env.PORT || 8878;
+const app = express();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const empregadas = [];
+
+app.get('/', (req,res) => res.json(empregadas));
+
+app.post('/', (req, res) => {
+  const { nome, cep, disponibilidade } = req.body;
+  const empregada = { id:uuid(), nome, cep, disponibilidade };
+
+  empregadas.push(empregada);
+  return res.json(empregada);
+})
+
+app.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = empregadas.findIndex(empregada => empregada.id === id);
+  empregadas.splice(index,1);
+  if(index > 0) {
+    return res.send()
+  }else{
+    res.status(400).json({erro : "deu erro"})
+  }
+})
+
+
+
+app.listen(port,() => console.log('server rodando'))
